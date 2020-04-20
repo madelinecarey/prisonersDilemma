3268470116291a6ab2766da60f58f2b4504155d1
madelinecarey <62911785+madelinecarey@users.noreply.github.com>
2020-04-20 14:42:27 -0400

    Update CNPC.java

--- CNPC.java
+++ CNPC.java
@@ -2,10 +2,9 @@ package prisonersDilemma;
 import java.awt.Color;
 
 public class CNPC extends oilCompany {
-	
-	public CNPC(Location myLocation, World myWorld, int myInfluence, double myPrice, int myQuantity) {
-		super(392000000, myLocation, Color.RED, myWorld, myInfluence, "China", true, myPrice, myQuantity);
-	}
+		
+	public CNPC(Location myLocation, World myWorld, double myMarketShare, double myPrice, int myQuantity) {
+		super(392000000, myLocation, Color.RED, myWorld, 10.4, "China", true, myPrice, myQuantity);
 
 }
 
