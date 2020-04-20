41b9fe7c98c06639369a39ad3f48f3b26c168e54
madelinecarey <62911785+madelinecarey@users.noreply.github.com>
2020-04-20 14:49:49 -0400

    Update petrobras.java

--- petrobras.java
+++ petrobras.java
@@ -3,8 +3,8 @@ import java.awt.Color;
 
 public class petrobras extends oilCompany {
 	
-	public petrobras(Location myLocation, World myWorld, int myInfluence, double myPrice, int myQuantity) {
-		super(950000000, myLocation, Color.YELLOW, myWorld, myInfluence, "Brazil", true, myPrice, myQuantity);
+	public petrobras(Location myLocation, World myWorld, double myMarketShare, double myPrice, int myQuantity) {
+		super(950000000, myLocation, Color.YELLOW, myWorld, 2.5, "Brazil", true, myPrice, myQuantity);
 	}
 
 }
