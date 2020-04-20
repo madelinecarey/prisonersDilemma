945eac715e0d553a2de40ebf1cf5000bb1ce28b0
madelinecarey <62911785+madelinecarey@users.noreply.github.com>
2020-04-20 14:46:41 -0400

    Update Shell.java

--- Shell.java
+++ Shell.java
@@ -2,9 +2,9 @@ package prisonersDilemma;
 import java.awt.Color;
 
 public class Shell extends oilCompany {
-	
-	public Shell(Location myLocation, World myWorld, int myInfluence, double myPrice, int myQuantity) {
-		super(396000000, myLocation, Color.BLUE, myWorld, myInfluence, "United Kingdom", true, myPrice, myQuantity);
+		
+	public Shell(Location myLocation, World myWorld, double myMarketShare, double myPrice, int myQuantity) {
+		super(396000000, myLocation, Color.BLUE, myWorld, 10.4, "United Kingdom", true, myPrice, myQuantity);
 	}
 	
 }
