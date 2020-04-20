b5ef36a6e20e3c793ad920deaf126ee4b0f8b3ba
madelinecarey <62911785+madelinecarey@users.noreply.github.com>
2020-04-20 14:49:30 -0400

    Update pemex.java

--- pemex.java
+++ pemex.java
@@ -3,9 +3,10 @@ import java.awt.Color;
 
 public class pemex extends oilCompany {
 	
-	public pemex(Location myLocation, World myWorld, int myInfluence, double myPrice, int myQuantity) {
-		super(85000000, myLocation, COLOR.DARK_GRAY, myWorld, myInfluence, "Mexico", true, myPrice, myQuantity);
+	public pemex(Location myLocation, World myWorld, double myMarketShare, double myPrice, int myQuantity) {
+		super(85000000, myLocation, COLOR.DARK_GRAY, myWorld, 2, "Mexico", true, myPrice, myQuantity);
 	}
+
 
 }
 
