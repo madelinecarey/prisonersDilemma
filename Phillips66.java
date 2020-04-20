68833ba43f18f1f9c072acf8881721a89f360a78
madelinecarey <62911785+madelinecarey@users.noreply.github.com>
2020-04-20 14:45:23 -0400

    Update Phillips66.java

--- Phillips66.java
+++ Phillips66.java
@@ -3,8 +3,7 @@ import java.awt.Color;
 
 public class Phillips66 extends oilCompany {
 	
-	public Phillips66(Location myLocation, World myWorld, int myInfluence, double myPrice, int myQuantity) {
-		super(102000000, myLocation, Color.CYAN, myWorld, myInfluence, "United States", false, myPrice, myQuantity);
+	public Phillips66(Location myLocation, World myWorld, double myMarketShare, double myPrice, int myQuantity) {
+		super(102000000, myLocation, Color.CYAN, myWorld, 2.9, "United States", false, myPrice, myQuantity);
 	}
-
 }
