03e25d8b242b1f82ceeec6c3ef8436a98200eefc
madelinecarey <62911785+madelinecarey@users.noreply.github.com>
2020-04-20 14:46:16 -0400

    Update SaudiAramco.java

--- SaudiAramco.java
+++ SaudiAramco.java
@@ -3,8 +3,8 @@ import java.awt.Color;
 
 public class SaudiAramco extends oilCompany {
 	
-	public SaudiAramco(Location myLocation, World myWorld, int myInfluence, double myPrice, int myQuantity) {
-		super(355000000, myLocation, Color.GREEN, myWorld, myInfluence, "Saudi Arabia", true, myPrice, myQuantity);
+	public SaudiAramco(Location myLocation, World myWorld, double myMarketShare, double myPrice, int myQuantity) {
+		super(355000000, myLocation, Color.GREEN, myWorld, 9.4, "Saudi Arabia", true, myPrice, myQuantity);
 	}
 
 }
