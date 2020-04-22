d1874de1570c69feaaf466341038d56395b14693
wigglydtrain <dthomas20@deerfield.edu>
2020-04-22 10:48:06 -0700

    for class again

---
+++ antiTrustLaw.java
@@ -0,0 +1,16 @@
+package prisonersDilemma;
+
+import java.util.*;
+
+public class antiTrustLaw {
+	
+	public antiTrustLaw(String country, ArrayList<Company> underJurisdiction) {
+		super();
+		this.country = country;
+		this.underJurisdiction = underJurisdiction;
+	}
+	protected String country;
+	protected ArrayList<Company> underJurisdiction;
+	
+	
+}
