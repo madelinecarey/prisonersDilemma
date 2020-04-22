6eab7c97045b0918c4f14bc5ee2674c7863eeb94
wigglydtrain <dthomas20@deerfield.edu>
2020-04-22 10:54:53 -0700

    more for class

--- CollusiveAgreement.java
+++ CollusiveAgreement.java
@@ -1,6 +1,14 @@
 
+import java.util.*;
+
 public class CollusiveAgreement {
 
-	
+	public CollusiveAgreement(String name, ArrayList<Company> companiesAgreed) {
+		super();
+		this.name = name;
+		this.companiesAgreed = companiesAgreed;
+	}
+	protected String name;
+	protected ArrayList<Company> companiesAgreed;
 	
 }
